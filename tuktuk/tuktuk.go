a622921a2c55654dd1f13c82702e1e538a3518cd
Passakorn Tharnwanitchakarn <passakorn.tkn@gmail.com>
2025-08-26 16:33:09 +0700

    add waitTime function and corresponding tests

--- tuktuk/tuktuk.go
+++ tuktuk/tuktuk.go
@@ -5,3 +5,7 @@ import "math"
 func distance(raw float64) float64 {
 	return math.Ceil(raw*2) / 2
 }
+
+func waitTime(raw float64) float64 {
+	return math.Ceil(raw)
+}
--- tuktuk/tuktuk_test.go
+++ tuktuk/tuktuk_test.go
@@ -35,3 +35,29 @@ func TestDistance(t *testing.T) {
 		assert.Equal(t, 20.0, distance(19.9))
 	})
 }
+
+func TestWaitTime(t *testing.T) {
+	t.Run("should return 0 when input is 0", func(t *testing.T) {
+		assert.Equal(t, 0.0, waitTime(0.0))
+	})
+
+	t.Run("should return 1 when input is 0.1", func(t *testing.T) {
+		assert.Equal(t, 1.0, waitTime(0.1))
+	})
+
+	t.Run("should return 1 when input is 0.5", func(t *testing.T) {
+		assert.Equal(t, 1.0, waitTime(0.5))
+	})
+
+	t.Run("should return 1 when input is 0.6", func(t *testing.T) {
+		assert.Equal(t, 1.0, waitTime(0.6))
+	})
+
+	t.Run("should return 1.0 when input is 1.0", func(t *testing.T) {
+		assert.Equal(t, 1.0, waitTime(1.0))
+	})
+
+	t.Run("should return 2 when input is 1.2", func(t *testing.T) {
+		assert.Equal(t, 2.0, waitTime(1.2))
+	})
+}
